326aa6ed399fc4adbaafe26a17a1617a36206780
Esteban Jelicich <jelicich.e@gmail.com>
2017-02-21 03:08:03 -0300

    mobile adjustments

--- assets/js/main.js
+++ assets/js/main.js
@@ -606,21 +606,62 @@ portfolio.Lab = $.extend(true, {}, portfolio.Works, {
 
 portfolio.Common = {
     wheelCount: 0,
+    swipeCount: 0,
     isModalOpen: false,
+    modalOpen: null,
 
     init : function() 
     {   
+        var t = this;
         this.scrollTo();
-        this.onWheelMove();
-        this.onMobileScroll();
-        
-        $modal = $('.modal').on('shown.bs.modal', function(){
+
+        if(this.isMobile()) {
+            t.onMobileSwipe();
+            t.addMultipleEventListener(window, 'touchmove touchcancel', function(event) {
+                if(!portfolio.Common.isModalOpen) { 
+                    event.preventDefault();
+                }
+            })
+
+            //scroll to contact after input blur 
+            $('#contact input, #contact textarea').blur(function() {
+                var top = $('#contact').offset().top;
+                $('html, body').stop().animate({
+                    scrollTop: top
+                }, 200);
+            });
+        } else {
+            this.onWheelMove();
+        }
+
+        $modal = $('.modal').on('shown.bs.modal', function(e){
             portfolio.Common.isModalOpen = true;
+            portfolio.Common.modalOpen = this;
         })
 
-        $modal = $('.modal').on('hidden.bs.modal', function(){
+        $modal = $('.modal').on('hidden.bs.modal', function(e){
             portfolio.Common.isModalOpen = false;
+            portfolio.Common.modalOpen = null;
         })
+    },
+
+    addMultipleEventListener: function(element, eventNames, listener) {
+        var events = eventNames.split(' ');
+        for (var i=0, iLen=events.length; i<iLen; i++) {
+            element.addEventListener(events[i], listener, false);
+        }
+    },
+
+    isMobile: function() {
+        var check = false;
+        (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+        return check;
+    },
+
+    isMobileAndTablet: function() {
+        var check = false;
+        (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+        return check;
     },
 
     scrollTo: function(){
@@ -652,11 +693,7 @@ portfolio.Common = {
             delay = true;
             setTimeout(function(){delay = false; portfolio.Common.wheelCount = 0;}, 500)
 
-            
-        
             var wd = event.originalEvent.wheelDelta || -event.originalEvent.detail;
-
-            
 
             var a = document.querySelectorAll('section.portfolio-section');
 
@@ -682,55 +719,55 @@ portfolio.Common = {
         }          
       });
     },
-    
-    onMobileScroll: function() {
+
+    onMobileSwipe: function() {
         var delay = false;
-        var lastY;
-        $('body').on({
-            'touchmove': function(event) { 
-                if(portfolio.Common.isModalOpen) return;
-                //debugger;
-                event.preventDefault();
-                portfolio.Common.wheelCount++;
-                
-                //console.log(portfolio.Common.wheelCount);
-                
-                if(portfolio.Common.wheelCount >= 3) {
-                    if(delay) return;
-
-                    delay = true;
-                    setTimeout(function(){delay = false; portfolio.Common.wheelCount = 0;}, 500);
-
-                    var a = document.querySelectorAll('section.portfolio-section');
-
-                    var currentY = event.originalEvent.touches[0].clientY;
-
-                    if(currentY > lastY) {
-                      for(var i = 0 ; i < a.length ; i++) {
-                        var t = a[i].getClientRects()[0].top;
-                        if(t >= 40) break;
-                      }
-                    }
-                    else {
-                      for(var i = a.length-1 ; i >= 0 ; i--) {
-                        var t = a[i].getClientRects()[0].top;
-                        if(t < -20) break;
-                      }
-                    }
-
-                    lastY = currentY;
-
-                    //debugger;
-                    var top = (a[i]) ? a[i].offsetTop : null;
-                    if(top != null) {
-                        $('html,body').animate({
-                          scrollTop: top
-                        },1000);
-                    }
-                } // Replace this with your code.
+        $('body').swipe(function(direction, offset) {
+            if(portfolio.Common.isModalOpen) {
+                // var $modal = $(portfolio.Common.modalOpen).find('.modal-dialog');
+                // $modal.css('transform','translate(0,' + (offset.y * -1) + 'px)');
+                // console.log(offset.y)
+                return;
             }
+
+            portfolio.Common.swipeCount++;
+
+            if(Math.abs(offset.y) > 20) {
+
+                if(delay) return;
+                delay = true;
+                setTimeout(function(){delay = false; }, 250)
+
+                var a = document.querySelectorAll('section.portfolio-section');
+                
+                //swipe up
+                if(offset.y > 0) {
+                  for(var i = 0 ; i < a.length ; i++) {
+                    var t = a[i].getClientRects()[0].top;
+                    if(t >= 40) break;
+                  }
+                }
+                //swipe down
+                else if(offset.y < 0) {
+                  for(var i = a.length-1 ; i >= 0 ; i--) {
+                    var t = a[i].getClientRects()[0].top;
+                    if(t < -20) break;
+                  }
+                }
+
+                //debugger;
+                var top = (a[i]) ? a[i].offsetTop : null;
+                if(top != null) {
+                    $('html,body').animate({
+                      scrollTop: top
+                    },1000);
+                }
+            }
+                
         });
-    } 
+    },
+    
+    
 }
 
 portfolio.EventHandler = {
@@ -776,12 +813,12 @@ portfolio.EventHandler = {
 
         ( function( $ ) {
             // Init Skrollr
-            var s = skrollr.init({
-                render: function(data) {
-                    //Debugging - Log the current scroll position.
-                    //console.log(data.curTop);
-                }
-            });
+            // var s = skrollr.init({
+            //     render: function(data) {
+            //         //Debugging - Log the current scroll position.
+            //         //console.log(data.curTop);
+            //     },
+            // });
         } )( jQuery );
     });
 }
